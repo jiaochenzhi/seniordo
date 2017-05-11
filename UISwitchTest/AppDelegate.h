655c923f4c184b0445c4cf0a7a635eb48c634d74
倪菁华 <NoraNi@>
2017-05-11 20:33:59 +0800

    Initial Commit

---
+++ UISwitchTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISwitchTest
+//
+//  Created by 倪菁华 on 2017/5/11.
+//  Copyright © 2017年 SeniorDo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISwitchTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISwitchTest
+//
+//  Created by 倪菁华 on 2017/5/11.
+//  Copyright © 2017年 SeniorDo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
